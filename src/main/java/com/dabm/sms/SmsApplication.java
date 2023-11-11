cb4400a3fc00bedf0654051499d16a2422f3f1be
Jacques-Michel Adebayo <jacquesajmo@gmail.com>
2023-11-11 15:40:09 +0100

    build: init project

---
+++ src/main/java/com/dabm/sms/SmsApplication.java
@@ -0,0 +1,16 @@
+package com.dabm.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@SpringBootApplication
+@EnableWebFlux
+
+public class SmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsApplication.class, args);
+	}
+
+}
