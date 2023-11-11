4e835e0df7e680d802c11feff73d87e3141f953a
Jacques-Michel Adebayo <jacquesajmo@gmail.com>
2023-11-11 15:43:07 +0100

    build: config mongo

--- src/main/java/com/dabm/sms/config/MongoConfig.java
+++ src/main/java/com/dabm/sms/config/MongoConfig.java
@@ -3,10 +3,8 @@ package com.dabm.sms.config;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
-import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @Configuration
@@ -28,11 +26,4 @@ public class MongoConfig extends AbstractReactiveMongoConfiguration {
     protected String getDatabaseName() {
         return dbName;
     }
-
-    @Bean
-    public ReactiveMongoTemplate reactiveMongoTemplate() {
-        return new ReactiveMongoTemplate(reactiveMongoClient(), getDatabaseName());
-    }
-
-
 }
